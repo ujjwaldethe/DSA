82b959690a942fc250c9994693418d96cc430f12
ujjwal dethe <ujwaldethe518@gmail.com>
2024-10-26 01:19:57 +0530

    commit

--- src/Strings/largestStringLexicographical.java
+++ src/Strings/largestStringLexicographical.java
@@ -11,5 +11,22 @@ public class largestStringLexicographical {
             }
         }
         System.out.println("Largest String : "+largest);
-    }
+        String name = "hi, i am shradha";
+        StringBuilder sb = new StringBuilder("");
+        char ch = Character.toUpperCase(name.charAt(0));
+        sb.append(ch);
+        for(int i=1; i<name.length();i++)
+        {
+            if(name.charAt(i)==' '&& i<name.length()-1){
+                sb.append(name.charAt(i));
+                i++;
+                sb.append(Character.toUpperCase(name.charAt(i)));
+            }
+            else{
+                sb.append(name.charAt(i));
+            }
+        }
+        System.out.println(sb.toString());
+        }
+
 }
